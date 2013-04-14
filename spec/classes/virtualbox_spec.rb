25d0502ae44bf2147a559bfd9d8099139c104abf
Ben Haan <benhaan@gmail.com>
2013-04-14 17:49:00 -0500

    bumps version to 4.2.12

--- spec/classes/virtualbox_spec.rb
+++ spec/classes/virtualbox_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'virtualbox' do
   it do
     should contain_package('VirtualBox').with({
-      :source   => 'http://download.virtualbox.org/virtualbox/4.2.10/VirtualBox-4.2.10-84104-OSX.dmg',
+      :source   => 'http://download.virtualbox.org/virtualbox/4.2.12/VirtualBox-4.2.12-84980-OSX.dmg',
       :provider => 'pkgdmg'
     })
   end
