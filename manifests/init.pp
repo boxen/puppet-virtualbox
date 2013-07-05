c1f6aa7af6be286fa077c931683d4f29915a58ac
Felipe Oliveira <felipe.oliveira@abril.com.br>
2013-07-05 10:39:34 -0300

    Updating VirtualBox version to 4.2.16

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,7 +6,7 @@
 class virtualbox {
   package { 'VirtualBox':
     provider => 'pkgdmg',
-    source   => 'http://download.virtualbox.org/virtualbox/4.2.14/VirtualBox-4.2.14-86644-OSX.dmg'
+    source   => 'http://download.virtualbox.org/virtualbox/4.2.16/VirtualBox-4.2.16-86992-OSX.dmg'
   }
 }
 
