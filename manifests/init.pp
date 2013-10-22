2d50ebbb764c5b3a848184b8849c4eca33b8e4d5
aripalo <ari@aripalo.fi>
2013-10-22 14:13:51 +0300

    virtual box processes need to be killed before new version install

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,10 +4,18 @@
 #
 #   include virtualbox
 class virtualbox {
+
+  exec { 'Kill Virtual Box Processes':
+    command => 'killall -9 VBoxXPCOMIPCD && killall -9 VBoxSVC && killall -9 VBoxHeadless',
+    path    => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
+    #refreshonly => true,
+  }
+
   package { 'VirtualBox-4.3.0-89960':
+    ensure   => installed,
     provider => 'pkgdmg',
     source   => 'http://download.virtualbox.org/virtualbox/4.3.0/VirtualBox-4.3.0-89960-OSX.dmg',
-    ensure   => installed,
+    require  => Exec['Kill Virtual Box Processes'],
   }
 }
 
--- spec/classes/virtualbox_spec.rb
+++ spec/classes/virtualbox_spec.rb
@@ -3,9 +3,10 @@ require 'spec_helper'
 describe 'virtualbox' do
   it do
     should contain_package('VirtualBox-4.3.0-89960').with({
+      :ensure   => 'installed',
       :source   => 'http://download.virtualbox.org/virtualbox/4.3.0/VirtualBox-4.3.0-89960-OSX.dmg',
       :provider => 'pkgdmg',
-      :ensure   => installed,
+      :require  => 'Exec[Kill Virtual Box Processes]',
     })
   end
 end
