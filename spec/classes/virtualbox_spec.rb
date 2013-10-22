41014b32bee4d25f62fa96c647f62cb1146d1df9
aripalo <ari@aripalo.fi>
2013-10-22 13:43:21 +0300

    add tests

--- spec/classes/virtualbox_spec.rb
+++ spec/classes/virtualbox_spec.rb
@@ -2,9 +2,10 @@ require 'spec_helper'
 
 describe 'virtualbox' do
   it do
-    should contain_package('VirtualBox').with({
-      :source   => 'http://download.virtualbox.org/virtualbox/4.2.18/VirtualBox-4.2.18-88780-OSX.dmg',
-      :provider => 'pkgdmg'
+    should contain_package('VirtualBox-4.3.0-89960').with({
+      :source   => 'http://download.virtualbox.org/virtualbox/4.3.0/VirtualBox-4.3.0-89960-OSX.dmg',
+      :provider => 'pkgdmg',
+      :ensure   => installed,
     })
   end
 end
