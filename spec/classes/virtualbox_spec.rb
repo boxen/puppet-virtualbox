5db21c94ab2fa5f82945c511e55a40b3c58d9fde
aripalo <ari@aripalo.fi>
2013-11-05 22:25:24 +0200

    fix virtualbox version in spec.rb

--- spec/classes/virtualbox_spec.rb
+++ spec/classes/virtualbox_spec.rb
@@ -9,7 +9,7 @@ describe 'virtualbox' do
     })
   end
   it do
-    should contain_package('VirtualBox-4.3.0-89960').with({
+    should contain_package('VirtualBox-4.3.2-90405').with({
       :ensure   => 'installed',
       :source   => 'http://download.virtualbox.org/virtualbox/4.3.2/VirtualBox-4.3.2-90405-OSX.dmg',
       :provider => 'pkgdmg',
