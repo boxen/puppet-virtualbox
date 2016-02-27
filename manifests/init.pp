9169ab51eb495481fdef74a87321edf3c7d7ca46
Florian Schaper <florian.schaper@hmmh.de>
2016-02-27 02:32:54 +0100

    bumped virtualbox version

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,8 +4,8 @@
 #
 #   include virtualbox
 class virtualbox (
-  $version = '4.3.14',
-  $patch_level = '95030'
+  $version = '5.0.14',
+  $patch_level = '105127'
 ) {
 
   exec { 'Kill Virtual Box Processes':
--- spec/classes/virtualbox_spec.rb
+++ spec/classes/virtualbox_spec.rb
@@ -9,9 +9,9 @@ describe 'virtualbox' do
     })
   end
   it do
-    should contain_package('VirtualBox-4.3.14-95030').with({
+    should contain_package('VirtualBox-5.0.14-105127').with({
       :ensure   => 'installed',
-      :source   => 'http://download.virtualbox.org/virtualbox/4.3.14/VirtualBox-4.3.14-95030-OSX.dmg',
+      :source   => 'http://download.virtualbox.org/virtualbox/5.0.14/VirtualBox-5.0.14-105127-OSX.dmg',
       :provider => 'pkgdmg',
       :require  => 'Exec[Kill Virtual Box Processes]',
     })
